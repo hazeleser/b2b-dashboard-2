dfe4a6a88c10b835fcf717643e392755857dee45
hazeleser <hazeleser@gmail.com>
2025-11-14 11:17:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly express as px
+import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
 import matplotlib.pyplot as plt
